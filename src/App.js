70dd6e902d55f8d386111bbee52ded4305d694db
Kumar Krishnamoorthy <kumar.softindies@gmail.com>
2018-07-23 17:28:55 +0530

    Initial commit -- CSS is OFF!

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Table6 from './components/Table6';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+	<Table6/>
+      </div>
+    );
+  }
+}
+
+export default App;
